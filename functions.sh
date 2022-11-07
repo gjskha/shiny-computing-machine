96cfb39546c7b1ca4bdea34eea46983e067c54b0
gjskha <gjskha@gmail.com>
2022-11-07 12:53:45 -0800

    Create functions.sh

---
+++ functions.sh
@@ -0,0 +1,29 @@
+function archive_files() {
+   bound=$1
+   destdir=$2
+   ls -l --time-style +%s | while read line; do
+       echo $line |  awk '{print $6" "$7}' | while read age name; do
+           if [[ $age -lt $bound ]]; then
+               if [ ! -d $destdir ]; then
+                   mkdir $destdir
+                   if [ $? -ne 0 ]; then
+                      echo "Problem creating $destdir."
+                      return
+                   fi
+               fi
+               echo "moving $name to $destdir"
+               mv -i $name $destdir/.
+               if [ $? -ne 0 ]; then
+                   echo "problem moving '$name' to $destdir/."
+               fi
+           fi
+       done
+   done
+}
+
+function d2e() {
+    # d2e date
+    if [ ! -z $1 ]; then
+        date -j -f "%a %b %d %T %Z %Y" "`$*`" "+%s"
+    fi
+}
