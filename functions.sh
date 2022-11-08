77617d51659789856387662895c0fef7de93b9ce
gjskha <gjskha@gmail.com>
2022-11-08 09:15:47 -0800

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -2,12 +2,15 @@ function archive_files() {
    bound=$1
    destdir=$2
    ls -l --time-style +%s | awk '{print $6" "$7}' | while read age name; do
-       echo age is $age and name is $name
        if [[ $age -lt $bound ]]; then
-           echo "moving $name to $destdir"
            if [ ! -d $destdir ]; then
                mkdir $destdir
-           fi  
+               if [ $? -ne 0 ]; then
+                   echo "Problem creating $destdir."
+                   return
+               fi  
+           fi
+           echo "moving $name to $destdir"
            mv -i $name $destdir/.
            if [ $? -ne 0 ]; then
               echo "problem moving '$name' to $destdir/."
