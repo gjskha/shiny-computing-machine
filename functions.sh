29a32473222cccba1db7f0f12bdeb8c166b11726
gjskha <gjskha@gmail.com>
2022-11-08 09:08:22 -0800

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,23 +1,18 @@
 function archive_files() {
    bound=$1
    destdir=$2
-   ls -l --time-style +%s | while read line; do
-       echo $line |  awk '{print $6" "$7}' | while read age name; do
-           if [[ $age -lt $bound ]]; then
-               if [ ! -d $destdir ]; then
-                   mkdir $destdir
-                   if [ $? -ne 0 ]; then
-                      echo "Problem creating $destdir."
-                      return
-                   fi
-               fi
-               echo "moving $name to $destdir"
-               mv -i $name $destdir/.
-               if [ $? -ne 0 ]; then
-                   echo "problem moving '$name' to $destdir/."
-               fi
-           fi
-       done
+   ls -l --time-style +%s | awk '{print $6" "$7}' | while read age name; do
+       echo age is $age and name is $name
+       if [[ $age -lt $bound ]]; then
+           echo "moving $name to $destdir"
+           if [ ! -d $destdir ]; then
+               mkdir $destdir
+           fi  
+           mv -i $name $destdir/.
+           if [ $? -ne 0 ]; then
+              echo "problem moving '$name' to $destdir/."
+           fi  
+       fi  
    done
 }
 
